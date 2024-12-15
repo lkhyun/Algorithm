275674122d58a115cc94d18fa3e457220929629c
lkhyun <102892446+lkhyun@users.noreply.github.com>
2024-12-15 19:21:32 +0900

    [level 3] Title: 입국심사, Time: 493.96 ms, Memory: 14.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -0,0 +1,19 @@
+def solution(n, times):
+    answer = 0
+    left = 1
+    right = max(times)*n
+    while left <= right:
+        capacity = 0
+        mid = (left + right) // 2
+        for time in times:
+            capacity += mid // time
+            if capacity >= n:
+                break
+                
+        if capacity >= n:
+            answer = mid
+            right = mid - 1
+        elif capacity < n:
+            left = mid + 1
+        
+    return answer
