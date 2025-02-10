01b720867911123b5e575deaf66f75c1e8ae39eb
lkhyun <102892446+lkhyun@users.noreply.github.com>
2025-02-10 13:44:08 +0900

    [Bronze II] Title: Hashing, Time: 108 ms, Memory: 14332 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -8,13 +8,17 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int L = Integer.parseInt(br.readLine());
         char[] str = br.readLine().toCharArray();
-        int hash = 0;
-
+        int r = 31;
+        int M = 1234567891;
+        long hash = 0;
+        long power = 1;
         for(int i=0;i<L;i++){
             int alphabet= (str[i]-'a')+1;
-            hash += alphabet*(int)Math.pow(31,i);
+            hash += (alphabet*power)%M;
+            hash %=M;
+            power = (power*r)%M;
         }
-        
+
         bw.write(hash+"");
         bw.flush();
     }
