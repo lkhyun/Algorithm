3a511050fb3cf123882e1d2619d12fd1b3ac3d72
lkhyun <102892446+lkhyun@users.noreply.github.com>
2025-05-02 16:56:25 +0900

    [Gold V] Title: LCS, Time: 128 ms, Memory: 18284 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static int[][] LCS;
+
+    public static void main(String[] args) throws Exception {
+        char[] A = br.readLine().toCharArray();
+        char[] B = br.readLine().toCharArray();
+        LCS = new int[A.length + 1][B.length + 1];
+        for (int i = 1; i <= A.length; i++) {
+            for (int j = 1; j <= B.length; j++) {
+                if (A[i - 1] == B[j - 1]) {
+                    LCS[i][j] = LCS[i - 1][j - 1] + 1;
+                } else {
+                    LCS[i][j] = Math.max(LCS[i - 1][j], LCS[i][j - 1]);
+                }
+            }
+        }
+        bw.write(LCS[A.length][B.length] + "");
+        bw.close();
+    }
+}
